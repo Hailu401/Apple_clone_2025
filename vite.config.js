eeeb9e55762596d7fd0ff9a9fc21e16c29d5f996
Hailu401 <123373063+Hailu401@users.noreply.github.com>
2025-05-21 01:02:06 +0300

    final commt

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Apple_clone_2025/",
   server: {
     port: 5005,
   },
